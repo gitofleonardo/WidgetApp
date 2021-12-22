4c0b0ca7554d1dca881f7bc2cdea577c293f3dc3
Leonardo <31275062+gitofleonardo@users.noreply.github.com>
2021-12-22 21:10:29 +0800

    canvas can clear now(works not quite well)

--- app/src/main/java/com/example/widgetapp/draw/CanvasActivity.kt
+++ app/src/main/java/com/example/widgetapp/draw/CanvasActivity.kt
@@ -1,13 +1,12 @@
 package com.example.widgetapp.draw
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.widgetapp.BaseActivity
-import com.example.widgetapp.R
 import com.example.widgetapp.databinding.ActivityCanvasBinding
+import com.example.widgetapp.view.CanvasMode
 
 class CanvasActivity : BaseActivity<ActivityCanvasBinding>() {
     override fun onInit(savedInstanceState: Bundle?) {
-
+        binding.clearModeEnabled.setOnCheckedChangeListener { _, isChecked -> binding.canvas.canvasMode = if (isChecked) CanvasMode.CLEAR else CanvasMode.DRAW }
     }
 }
--- app/src/main/java/com/example/widgetapp/view/CanvasView.kt
+++ app/src/main/java/com/example/widgetapp/view/CanvasView.kt
@@ -1,10 +1,7 @@
 package com.example.widgetapp.view
 
 import android.content.Context
-import android.graphics.Canvas
-import android.graphics.Color
-import android.graphics.Paint
-import android.graphics.Path
+import android.graphics.*
 import android.util.AttributeSet
 import android.view.MotionEvent
 import android.view.View
@@ -18,33 +15,57 @@ class CanvasView(context:Context,attrs:AttributeSet?) : View(context, attrs) {
     constructor(context: Context):this(context,null)
 
     private val mPath = Path()
+    private val mClearPath = Path()
     private val mPaint = Paint().apply {
         color = Color.BLACK
         strokeWidth = 5.0f
         style = Paint.Style.STROKE
     }
-    private var mMode = CanvasMode.DRAW
+    private val mClearPathPaint = Paint().apply {
+        color = Color.GRAY
+        strokeWidth = 5.0f
+        style = Paint.Style.STROKE
+    }
+    var canvasMode = CanvasMode.DRAW
     private var mPrevX = 0.0f
     private var mPrevY = 0.0f
+    private var mShouldNextQuad = false
 
     override fun onTouchEvent(event: MotionEvent): Boolean {
+        val path = when (canvasMode) {
+            CanvasMode.DRAW -> mPath
+            CanvasMode.CLEAR -> mClearPath
+        }
         when (event.action) {
             MotionEvent.ACTION_DOWN -> {
+                mShouldNextQuad = false
                 mPrevX = event.x
                 mPrevY = event.y
-                mPath.moveTo(mPrevX,mPrevY)
+                path.moveTo(mPrevX,mPrevY)
                 return true
             }
             MotionEvent.ACTION_MOVE -> {
-                val endX = (mPrevX + event.x) / 2
-                val endY = (mPrevY + event.y) / 2
-                mPath.quadTo(event.x,event.y,endX,endY)
-                mPrevX= event.x
-                mPrevY = event.y
-                invalidate()
-                return true
+                return if (!mShouldNextQuad){
+                    mPrevX = event.x
+                    mPrevY = event.y
+                    mShouldNextQuad = true
+                    true
+                }else{
+                    mShouldNextQuad = false
+                    path.quadTo(mPrevX,mPrevY,event.x,event.y)
+                    mPrevX= event.x
+                    mPrevY = event.y
+                    invalidate()
+                    true
+                }
             }
             MotionEvent.ACTION_UP,MotionEvent.ACTION_CANCEL -> {
+                if (canvasMode == CanvasMode.CLEAR){
+                    mClearPath.close()
+                    mPath.op(mClearPath,Path.Op.DIFFERENCE)
+                    mClearPath.reset()
+                    invalidate()
+                }
                 return true
             }
         }
@@ -53,6 +74,9 @@ class CanvasView(context:Context,attrs:AttributeSet?) : View(context, attrs) {
 
     override fun onDraw(canvas: Canvas) {
         canvas.drawPath(mPath,mPaint)
+        if (canvasMode == CanvasMode.CLEAR){
+            canvas.drawPath(mClearPath,mClearPathPaint)
+        }
     }
 
     fun clearCanvas(){
